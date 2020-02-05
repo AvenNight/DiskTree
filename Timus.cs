22de605758cd050437f24450209922ff9c1817dc
AvenNight <art-ibr@yandex.ru>
2020-02-06 05:01:06 +0700

    init

---
+++ Timus.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Folder
+{
+    public string Name { get; }
+    public Folder Root { get; }
+    public HashSet<Folder> Folders;
+
+    public Folder(string name, Folder root)
+    {
+        Name = name;
+        this.Root = root;
+        Folders = new HashSet<Folder>();
+    }
+}
+
+public static class FolderExtensions
+{
+    public static List<string> GetPaths(this Folder folder, List<string> result = null, string prefix = "")
+    {
+        if (result == null) result = new List<string>();
+        if (folder.Root != null) prefix += " ";
+        foreach (var f in folder.Folders.OrderBy(e => e.Name, StringComparer.Ordinal))
+        {
+            result.Add($"{prefix}{f.Name}");
+            GetPaths(f, result, prefix);
+        }
+        return result;
+    }
+}
+class Timus
+{
+    static void MainX()
+    {
+        int number = int.Parse(Console.ReadLine());
+        var input = new List<string>();
+        for (int i = 0; i < number; i++)
+            input.Add(Console.ReadLine());
+        foreach (var e in Solve(input))
+            Console.WriteLine(e);
+    }
+
+    public static List<string> Solve(List<string> input)
+    {
+        var root = new Folder("", null);
+        foreach (var path in input)
+        {
+            var prev = root;
+            foreach (var f in path.Split('\\'))
+            {
+                if (!prev.Folders.Any(e => e.Name == f))
+                {
+                    var newFolder = new Folder(f, prev);
+                    prev.Folders.Add(newFolder);
+                    prev = newFolder;
+                }
+                else
+                    prev = prev.Folders.FirstOrDefault(e => e.Name == f);
+            }
+        }
+        return root.GetPaths().ToList();
+    }
+}
